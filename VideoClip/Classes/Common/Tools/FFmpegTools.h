69b7b441277a2389067c0b06669565009be75540
hejeffery <553504116@qq.com>
2017-03-28 17:50:29 +0800

    调整

---
+++ VideoClip/Classes/Common/Tools/FFmpegTools.h
@@ -0,0 +1,17 @@
+//
+//  FFmpegTools.h
+//  VideoClip
+//
+//  Created by HeJeffery on 2017/3/28.
+//  Copyright © 2017年 HeJeffery. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/*!
+ @class
+ @abstract ffmpeg的工具
+ */
+@interface FFmpegTools : NSObject
+
+@end
---
+++ VideoClip/Classes/Common/Tools/FFmpegTools.m
@@ -0,0 +1,13 @@
+//
+//  FFmpegTools.m
+//  VideoClip
+//
+//  Created by HeJeffery on 2017/3/28.
+//  Copyright © 2017年 HeJeffery. All rights reserved.
+//
+
+#import "FFmpegTools.h"
+
+@implementation FFmpegTools
+
+@end
